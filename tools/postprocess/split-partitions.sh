fe67c591b5065153dd4459d36027df84623d6a14
onesaladleaf <180638599+onesaladleaf@users.noreply.github.com>
2026-07-31 00:34:28 +0300

    postprocess: fix root image naming

--- tools/postprocess/split-partitions.sh
+++ tools/postprocess/split-partitions.sh
@@ -6,13 +6,13 @@ mkdir -p $OUTPUT/images
 
 dd if=$esp_part of=$OUTPUT/images/fedora_esp.raw bs=1M
 dd if=$boot_part of=$OUTPUT/images/fedora_boot.raw bs=1M
-dd if=$root_part of=$OUTPUT/images/fedora_root.raw bs=1M
+dd if=$root_part of=$OUTPUT/images/fedora_rootfs.raw bs=1M
 
 sync
 
 # pad the last block to 4096 bytes, fastboot needs this
-if (( $(stat -c%s "$OUTPUT/images/fedora_root.raw") % 4096 != 0 )); then
-    dd if=/dev/zero bs=1 count=512 | tee -a $OUTPUT/images/fedora_root.raw
+if (( $(stat -c%s "$OUTPUT/images/fedora_rootfs.raw") % 4096 != 0 )); then
+    dd if=/dev/zero bs=1 count=512 | tee -a $OUTPUT/images/fedora_rootfs.raw
 fi
 
 chmod 666 $OUTPUT/images/*
