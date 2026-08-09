aa3ed837b43050034feb52c514ad7f16b47012d6
gmanka <gmankab@gmail.com>
2026-08-09 08:15:04 +0500

    qualcomm-sdm845: update uboot images
    new images are:
    
    - support btrfs
    - showing list of deployments available to boot

--- devices/qualcomm-sdm845/build-aux/artifacts.sh
+++ devices/qualcomm-sdm845/build-aux/artifacts.sh
@@ -2,4 +2,10 @@
 
 set -uexo pipefail
 
+which 7z
+
+mv $OUT_PATH/u-boot-*.zip ./
+7z x 'u-boot-*.zip'
+cp binaries/u-boot-oneplus-*.img $OUT_PATH/images/
+
 install -Dm 0755 $DEVICE_PATH/flash-scripts/flash.sh $OUT_PATH/flash.sh
